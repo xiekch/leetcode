9699e1006cb8291130acd35f82322688c93fe0a5
xiekch <xiekch@qq.com>
2019-03-04 22:43:26 +0800

    update

---
+++ JumpGame.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        for(int i=0,jumpLength=0;i<nums.size();i++,jumpLength--){
+            if(jumpLength<0)return false;
+            if(jumpLength<nums[i])jumpLength=nums[i];
+        }
+
+        return true;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> v={3,2,1,0,4};
+    cout<<s.canJump(v)<<endl;
+}
