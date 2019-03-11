e4e0d3275faf0059a1223ada106e1781a79c1a42
xiekch <xiekch@qq.com>
2019-03-11 22:28:39 +0800

    update

---
+++ SetMatrixZeroes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        set<int>row,column;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]==0){
+                    row.insert(i);
+                    column.insert(j);
+                }
+            }
+        }
+
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(row.count(i)||column.count(j))matrix[i][j]=0;
+            }
+        }
+    }
+};
--- UniquePathsV2.cpp
+++ UniquePathsV2.cpp
@@ -1,16 +1,14 @@
-//cannot be represented in type 'long int'
+// time limited
 class Solution {
 public:
-    long factorial(int n){
-        long result=1;
-        while(n>1){
-            result*=n;
-            n--;
-        }
-        return result;
+    long combination(int m,int n){
+        if(m==n)return 1;
+        else if(n==1)return m;
+        else return combination(m-1,n)+combination(m-1,n-1);
     }
 
     int uniquePaths(int m, int n) {
         // c(m,n)=c(m-1,n)+c(m-1,n-1)
+        return combination(m+n-2,m-1);
     }
 };
