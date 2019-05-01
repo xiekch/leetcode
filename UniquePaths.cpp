50ecf840fcfa551b4c18cdff261376b8572d889c
xiekch <xiekch@qq.com>
2019-05-01 16:17:13 +0800

    update

---
+++ UniquePaths.cpp
@@ -0,0 +1,15 @@
+#include <cstring>
+class Solution {
+public:
+    int arr[101][101];
+    int uniquePaths(int m, int n) {
+        memset(arr,0,sizeof(arr));
+        for(int i=1;i<=m;i++){
+            arr[i][1]=1;
+            for(int j=2;j<=n;j++){
+                arr[i][j]=arr[i-1][j]+arr[i][j-1];
+            }
+        }
+        return arr[m][n];
+    }
+};
