d31b2b68cf472108e7a3407f7742026649d13698
xiekch <xiekch@qq.com>
2020-03-18 10:59:12 +0800

    34. Find First and Last Position of Element in Sorted Array

---
+++ 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+    int findLeft(vector<int> &nums, int target) {
+        if (nums.size() == 0)
+            return -1;
+        int low = 0, high = nums.size() - 1;
+        int mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (low < nums.size() && nums[low] == target)
+            return low;
+        return -1;
+    }
+
+    int findRight(vector<int> &nums, int target) {
+        if (nums.size() == 0)
+            return -1;
+        int low = 0, high = nums.size() - 1;
+        int mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (nums[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (high >= 0 && nums[high] == target)
+            return high;
+        return -1;
+    }
+
+  public:
+    vector<int> searchRange(vector<int> &nums, int target) {
+        vector<int> res;
+        res.push_back(findLeft(nums, target));
+        res.push_back(findRight(nums, target));
+        return res;
+    }
+};
+
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+
+int main(int argc, char const *argv[]) {
+    int arr[] = {5, 7, 7, 8, 8, 10};
+    vector<int> test(arr, arr + 6);
+    Solution sol;
+    vector<int> res = sol.searchRange(test, 10);
+    cout << res[0] << " " << res[1] << endl;
+    return 0;
+}
