fbade23f9b2b1ee5a631cbdca1750094716d508b
xiekch <xiekch@qq.com>
2020-03-20 11:15:34 +0800

    39. Combination Sum v2

---
+++ 39_Combination_Sum_v2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// CSP
+// in fact, we dont need to count sum every time
+class Solution {
+    vector<vector<int>> ans;
+    int target;
+    vector<int> candidates;
+
+    void backtrack(vector<int> vec, int index, int remain) {
+        if (remain == 0) {
+            ans.push_back(vec);
+            return;
+        }
+        if (remain < 0)
+            return;
+        for (int i = index; i < candidates.size(); i++) {
+            vec.push_back(candidates[i]);
+            backtrack(vec, i, remain - candidates[i]);
+            vec.pop_back();
+        }
+    }
+
+  public:
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
+        this->target = target;
+        this->candidates = candidates;
+        vector<int> vec;
+        backtrack(vec, 0, target);
+        return this->ans;
+    }
+};
+// Input: candidates = [2,3,5], target = 8,
+// A solution set is:
+// [
+//   [2,2,2,2],
+//   [2,3,3],
+//   [3,5]
+// ]
+int main(int argc, char const *argv[]) {
+    int arr1[] = {2, 3, 5};
+    int arr2[] = {2, 3, 6, 7};
+    // vector<int> candidates(arr1, arr1 + 3);
+    vector<int> candidates(arr2, arr2 + 4);
+    Solution sol;
+    auto ans = sol.combinationSum(candidates, 8);
+
+    for (auto i : ans) {
+        for (auto j : i) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
