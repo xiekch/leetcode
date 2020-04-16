eaf1379515b37b80312e974882818f22d206be5c
xiekch <xiekch@qq.com>
2020-04-16 10:57:13 +0800

    238. Product of Array Except Self

---
+++ 238_Product_of_Array_Except_Self_v1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// output[i] is equal to the product of all the elements of nums except nums[i].
+// Note: Please solve it without division and in O(n).
+class Solution {
+  public:
+    vector<int> productExceptSelf(vector<int> &nums) {
+        vector<int> lToRi(nums.size(), 1), rToLe(nums.size(), 1);
+        for (int i = 1; i < nums.size(); i++) {
+            lToRi[i] = lToRi[i - 1] * nums[i - 1];
+        }
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            rToLe[i] = rToLe[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            lToRi[i] = lToRi[i] * rToLe[i];
+        }
+        return lToRi;
+    }
+};
+
+int main(int argc, char const *argv[]) {
+    Solution sol;
+    // vector<int> test = {1, 2, 3, 4};
+    vector<int> test = {1, 2};
+    auto i = sol.productExceptSelf(test);
+    for (auto j : i)
+        cout << j << " ";
+    cout << endl;
+    return 0;
+}
